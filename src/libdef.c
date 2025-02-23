f055d8e3d04d3b7249572baf75b65ef98fc82307
agayevhuseyn <huseynaga269@gmail.com>
2025-02-23 21:24:14 +0400

    library definitons added

---
+++ src/libdef.c
@@ -0,0 +1,85 @@
+#include "libdef.h"
+
+static inline const char*
+seal_type_name(seal_type type)
+{
+  switch (type) {
+    case SEAL_NULL:     return "null";
+    case SEAL_INT:      return "integer";
+    case SEAL_FLOAT:    return "float";
+    case SEAL_STRING:   return "string";
+    case SEAL_BOOL:     return "bool";
+    case SEAL_LIST:     return "list";
+    case SEAL_OBJECT:   return "object";
+    case SEAL_NUMBER:   return "number";
+    case SEAL_ITERABLE: return "iterable";
+    default:            return "SEAL TYPE NOT RECOGNIZED";
+  }
+}
+
+void seal_func_err(const char* libname, const char* func_name, const char* err)
+{
+  fprintf(stderr,
+          "seal: library \'%s\' error in \'%s\' function:\n%s\n",
+          libname,
+          func_name,
+          err);
+  exit(1);
+}
+
+void seal_check_args(const char* libname,
+                     const char* func_name,
+                     seal_type expected_types[],
+                     size_t type_size,
+                     sealobj* args[],
+                     size_t arg_size)
+{
+  if (arg_size != type_size) {
+    char err[ERR_LEN];
+    sprintf(err, "expected %lu arg%s, got %lu", type_size, type_size > 1 ? "s" : "\0", arg_size);
+    seal_func_err(libname, func_name, err);
+  }
+  for (int i = 0; i < type_size; i++) {
+    sealobj* arg = args[i];
+    switch (expected_types[i]) {
+      case SEAL_NULL:
+        if (IS_SEAL_NULL(arg)) continue;
+        break;
+      case SEAL_INT:
+        if (IS_SEAL_INT(arg)) continue;
+        break;
+      case SEAL_FLOAT:
+        if (IS_SEAL_FLOAT(arg)) continue;
+        break;
+      case SEAL_STRING:
+        if (IS_SEAL_STRING(arg)) continue;
+        break;
+      case SEAL_BOOL:
+        if (IS_SEAL_BOOL(arg)) continue;
+        break;
+      case SEAL_LIST:
+        if (IS_SEAL_LIST(arg)) continue;
+        break;
+      case SEAL_OBJECT:
+        if (IS_SEAL_OBJECT(arg)) continue;
+        break;
+      case SEAL_NUMBER:
+        if (IS_SEAL_NUMBER(arg)) continue;
+        break;
+      case SEAL_ITERABLE:
+        if (IS_SEAL_ITERABLE(arg)) continue;
+        break;
+    }
+
+    char err[ERR_LEN];
+    sprintf(err, "expected arg %d to be %s, got %s",
+           i + 1, seal_type_name(expected_types[i]), seal_type_name(arg->type));
+    seal_func_err(libname, func_name, err);
+  }
+}
+
+
+sealobj* get_obj_mem(sealobj* obj, const char* mem_name, seal_type type)
+{
+
+}
