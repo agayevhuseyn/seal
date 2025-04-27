12135fdcf3772582cdd189dac40b0830ba4d9e76
agayevhuseyn <huseynaga269@gmail.com>
2025-04-28 02:46:28 +0400

    compiler.c & compiler.h files added

---
+++ src/compiler.c
@@ -0,0 +1,77 @@
+#include "compiler.h"
+#include "op.h"
+#include "sealtypes.h"
+
+#define START_BYTECODE_CAP 1024
+
+#define PUSH(cout, byte) do { \
+    if (cout->bytecode_size >= cout->bytecode_cap) { \
+      cout->bytecodes = SEAL_REALLOC(cout->bytecodes, sizeof(uint8_t) * (cout->bytecode_cap *= 2)); \
+    } \
+    cout->bytecodes[cout->bytecode_size++] = byte; \
+  } while (0)
+
+#define PUSH_CONST(cout, val) ( \
+    /* check bounds */ \
+    *cout->const_pool_idx++ = val)
+
+void compile(cout_t* cout, ast_t* node)
+{
+  cout->bytecode_size = 0;
+  cout->bytecode_cap  = START_BYTECODE_CAP;
+  cout->const_pool_idx = cout->const_pool;
+  cout->bytecodes = SEAL_CALLOC(START_BYTECODE_CAP, sizeof(uint8_t));
+
+  compile_node(cout, node);
+
+  PUSH(cout, OP_PRINT);
+  PUSH(cout, OP_HALT); /* push halt opcode for termination */
+}
+static void compile_node(cout_t* cout, ast_t* node)
+{
+  switch (node->type) {
+    case AST_COMP: for (int i = 0; i < node->comp.stmt_size - 1; i++) { compile_node(cout, node->comp.stmts[i]); } break;
+    case AST_BINARY: compile_binary(cout, node); break;
+    case AST_NULL:
+    case AST_INT:
+    case AST_FLOAT:
+    case AST_STRING:
+    case AST_BOOL:
+      compile_val(cout, node);
+      break;
+  }
+}
+static void compile_binary(cout_t* cout, ast_t* node)
+{
+  compile_node(cout, node->binary.left);
+  compile_node(cout, node->binary.right);
+  
+  uint8_t opcode;
+  switch (node->binary.op_type) {
+    case TOK_PLUS : opcode = OP_ADD; break;
+    case TOK_MINUS: opcode = OP_SUB; break;
+    case TOK_MUL  : opcode = OP_MUL; break;
+    case TOK_DIV  : opcode = OP_DIV; break;
+    case TOK_MOD  : opcode = OP_MOD; break;
+  }
+
+  PUSH(cout, opcode);
+}
+static void compile_val(cout_t* cout, ast_t* node)
+{
+  svalue_t val;
+  switch (node->type) {
+    case AST_INT   : val.type = SEAL_INT;    val.as._int   = node->integer.val; break;
+    case AST_FLOAT : val.type = SEAL_FLOAT;  val.as._float = node->floating.val; break;
+    case AST_STRING: val.type = SEAL_STRING; val.as.string = node->string.val; break;
+    case AST_BOOL  : val.type = SEAL_BOOL;   val.as._bool  = node->boolean.val; break;
+    case AST_NULL  : val.type = SEAL_NULL; break;
+  }
+  PUSH(cout, OP_PUSH);
+  uint16_t idx = cout->const_pool_idx - cout->const_pool;
+  PUSH(cout, (uint8_t)idx >> 8);
+  PUSH(cout, (uint8_t)idx);
+  //printf("%ld\n", cout->const_pool_idx - cout->const_pool);
+  //printf("%ld\n", cout->const_pool_idx - cout->const_pool);
+  PUSH_CONST(cout, val);
+}
---
+++ src/compiler.h
@@ -0,0 +1,25 @@
+#ifndef SEAL_COMPILER_H
+#define SEAL_COMPILER_H
+
+#include "seal.h"
+#include "ast.h"
+#include "sealtypes.h"
+
+typedef struct cout cout_t;
+
+#define CONST_POOL_SIZE (0xFFFF + 1)    /* 65536 */
+
+struct cout {
+  uint8_t* bytecodes;     /* bytecode array */
+  size_t   bytecode_size; /* bytecode size */
+  size_t   bytecode_cap;  /* bytecode capacity */
+  svalue_t  const_pool[CONST_POOL_SIZE]; /* pool for constant values */
+  svalue_t* const_pool_idx; /* index for tracking constant pool values */
+};
+
+void compile(cout_t*, ast_t*); /* init cout and compile root node into bytecode */
+static void compile_node(cout_t*, ast_t*); /* compile any node into bytecode */
+static void compile_binary(cout_t*, ast_t*);
+static void compile_val(cout_t*, ast_t*);
+
+#endif /* SEAL_COMPILER_H */
