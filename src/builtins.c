a0ebbbf3e7354ef278adb77cb6aa9e7e7b136eeb
Hüseyn Ağayev <huseynaga269@gmail.com>
2025-06-03 23:10:56 +0400

    push & pop functions added, print, scan, len functions got polished

--- src/builtins.c
+++ src/builtins.c
@@ -1,4 +1,5 @@
 #include "builtins.h"
+#include "gc.h"
 
 #define BUILTIN_ERROR(...) do { \
   fprintf(stderr, __VA_ARGS__); \
@@ -8,36 +9,57 @@
 
 #define BUILTIN_FUNC_ARG_ERR(name, arg_i, given, expected) do { \
   BUILTIN_ERROR("in function \'%s\': expected arg %d to be \'%s\', got \'%s\'", \
-                name, arg_i, seal_type_name(given), seal_type_name(expected)); \
+                name, arg_i, seal_type_name(expected), seal_type_name(given)); \
 } while (0)
 
+static void __print_single(svalue_t s)
+{
+  switch (s.type) {
+  case SEAL_INT:
+    printf("%lld", AS_INT(s));
+    break;
+  case SEAL_FLOAT:
+    printf("%f", AS_FLOAT(s));
+    break;
+  case SEAL_STRING:
+    printf("%s", AS_STRING(s));
+    break;
+  case SEAL_BOOL:
+    printf("%s", AS_BOOL(s) ? "true" : "false");
+    break;
+  case SEAL_NULL:
+    printf("null");
+    break;
+  case SEAL_FUNC:
+    printf("function: %p", AS_FUNC(s).type == FUNC_BUILTIN ? (void*)&AS_FUNC(s).as.builtin.cfunc : (void*)&AS_FUNC(s).as.userdef.bytecode);
+    break;
+  case SEAL_LIST:
+    printf("[");
+    for (int i = 0; i < AS_LIST(s)->size; i++) {
+      if (IS_STRING(AS_LIST(s)->mems[i])) {
+        printf("\'");
+      }
+      __print_single(AS_LIST(s)->mems[i]);
+      if (IS_STRING(AS_LIST(s)->mems[i])) {
+        printf("\'");
+      }
+      if (i < AS_LIST(s)->size - 1)
+        printf(", ");
+    }
+    printf("]");
+    break;
+  default:
+    printf("UNRECOGNIZED DATA TYPE TO PRINT ");
+  }
+}
 svalue_t __seal_print(seal_byte argc, svalue_t* argv)
 {
   for (int i = 0; i < argc; i++) {
     svalue_t s = argv[i];
 
-    switch (s.type) {
-    case SEAL_INT:
-      printf("%lld ", AS_INT(s));
-      break;
-    case SEAL_FLOAT:
-      printf("%f ", AS_FLOAT(s));
-      break;
-    case SEAL_STRING:
-      printf("%s ", AS_STRING(s));
-      break;
-    case SEAL_BOOL:
-      printf("%s ", AS_BOOL(s) ? "true" : "false");
-      break;
-    case SEAL_NULL:
-      printf("null ");
-      break;
-    case SEAL_FUNC:
-      printf("function: %p ", AS_FUNC(s).type == FUNC_BUILTIN ? (void*)&AS_FUNC(s).as.builtin.cfunc : (void*)&AS_FUNC(s).as.userdef.bytecode);
-      break;
-    default:
-      printf("UNRECOGNIZED DATA TYPE TO PRINT\n");
-    }
+    __print_single(s);
+    if (i < argc - 1)
+      printf(" ");
   }
   printf("\n");
 
@@ -46,12 +68,14 @@ svalue_t __seal_print(seal_byte argc, svalue_t* argv)
 svalue_t __seal_scan(seal_byte argc, svalue_t* argv)
 {
   static char buf[BUFSIZ];
-  char *c = buf;
-  while ((*c = getchar()) != EOF && *c != '\n')
-    c++;
-
-  *c = '\0';
-  return SEAL_VALUE_STRING(buf);
+  size_t len = 0;
+  while ((buf[len] = getchar()) != EOF && buf[len] != '\n') {
+    len++;
+  }
+  buf[len] = '\0';
+  char* s = SEAL_CALLOC(len, sizeof(char));
+  strcpy(s, buf);
+  return SEAL_VALUE_STRING(s);
 }
 svalue_t __seal_exit(seal_byte argc, svalue_t* argv)
 {
@@ -63,10 +87,10 @@ svalue_t __seal_exit(seal_byte argc, svalue_t* argv)
 }
 svalue_t __seal_len(seal_byte argc, svalue_t* argv)
 {
-  if (!IS_STRING(argv[0]))
-    BUILTIN_FUNC_ARG_ERR("len", 0, VAL_TYPE(argv[0]), SEAL_STRING);
+  if (!IS_STRING(argv[0]) && !IS_LIST(argv[0]))
+    BUILTIN_FUNC_ARG_ERR("len", 0, VAL_TYPE(argv[0]), SEAL_STRING | SEAL_LIST);
 
-  return SEAL_VALUE_INT(strlen(AS_STRING(argv[0])));
+  return SEAL_VALUE_INT(IS_STRING(argv[0]) ? strlen(AS_STRING(argv[0])) : AS_LIST(argv[0])->size);
 }
 svalue_t __seal_int(seal_byte argc, svalue_t* argv)
 {
@@ -74,4 +98,22 @@ svalue_t __seal_int(seal_byte argc, svalue_t* argv)
     BUILTIN_FUNC_ARG_ERR("int", 0, VAL_TYPE(argv[0]), SEAL_STRING);
 
   return SEAL_VALUE_INT(atoi(AS_STRING(argv[0])));
+}
+svalue_t __seal_push(seal_byte argc, svalue_t* argv)
+{
+  if (!IS_LIST(argv[0]))
+    BUILTIN_FUNC_ARG_ERR("push", 0, VAL_TYPE(argv[0]), SEAL_LIST);
+
+  LIST_PUSH(argv[0], argv[1]);
+  gc_incref(argv[1]);
+  return SEAL_VALUE_NULL;
+}
+svalue_t __seal_pop(seal_byte argc, svalue_t* argv)
+{
+  if (!IS_LIST(argv[0]))
+    BUILTIN_FUNC_ARG_ERR("pop", 0, VAL_TYPE(argv[0]), SEAL_LIST);
+
+  svalue_t popped = AS_LIST(argv[0])->mems[--AS_LIST(argv[0])->size];
+  gc_decref_nofree(popped);
+  return popped;
 }
