79a243d52acce6628c859f18ec1b205e6ff4ef5e
Hüseyn Ağayev <huseynaga269@gmail.com>
2025-06-15 01:41:50 +0400

    scope struct to compile each scope separately

--- src/compiler.h
+++ src/compiler.h
@@ -13,7 +13,7 @@ typedef struct cout cout_t;
 #define LABEL_SIZE      (0xFFFF + 1)    /* 65536 */
 #define UNCOND_JMP_MAX_SIZE 1024
 
-#define PRINT_CONST_POOL(cout) for (int i = 0; i < cout.const_pool_ptr - cout.const_pool; i++) { \
+#define PRINT_CONST_POOL(cout) for (int i = 0; i < cout.const_pool_size; i++) { \
     if (i == 0) printf("CONST POOL START-------\n"); \
     svalue_t s = cout.const_pool[i]; \
     switch (s.type) { \
@@ -24,7 +24,7 @@ typedef struct cout cout_t;
         printf("%f\n", s.as._float); \
         break; \
       case SEAL_STRING: \
-        printf("\'%s\'\n", s.as.string); \
+        printf("\'%s\'\n", s.as.string->val); \
         break; \
       case SEAL_BOOL: \
         printf("%s\n", s.as._bool ? "true" : "false"); \
@@ -38,7 +38,7 @@ typedef struct cout cout_t;
       default: \
         printf("UNRECOGNIZED DATA TYPE TO PRINT\n"); \
     } \
-    if (i - 1 == cout.const_pool_ptr - cout.const_pool) printf("CONST POOL END-------\n"); \
+    if (i - 1 == cout.const_pool_size) printf("CONST POOL END-------\n"); \
   }
 
 struct bytechunk {
@@ -47,38 +47,58 @@ struct bytechunk {
   size_t cap;  /* bytecode capacity */
 };
 
+struct const_pool {
+  svalue_t *vals;
+  size_t size;
+  size_t cap;
+};
+
+struct label_pool {
+  seal_word *addrs;
+  size_t size;
+  size_t cap;
+};
+
+struct scope {
+  struct bytechunk bc;
+  struct const_pool cp;
+  struct label_pool lp;
+  hashmap_t loctable;
+};
+
 struct cout {
   struct bytechunk bc;     
   svalue_t*  const_pool; /* pool for constant values */
-  svalue_t* const_pool_ptr; /* pointer for tracking constant pool values */
+  size_t const_pool_size;
   seal_word* labels; /* array of labels to store */
-  seal_word* label_ptr; /* pointer for tracking labels */
+  size_t label_pool_size;
   size_t* skip_addr_offset_stack; /* stack for skip statements */
   size_t* stop_addr_offset_stack; /* stack for stop statements */
   size_t skip_size; /* skip statements size */
   size_t stop_size; /* stop statements size */
   seal_byte main_scope_local_size;
+  struct scope main_scope;
 };
 
 void compile(cout_t*, ast_t*); /* init cout and compile root node into bytecode */
-static void compile_node(cout_t*, ast_t*, hashmap_t*, struct bytechunk*); /* compile any node into bytecode */
-static void compile_if(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_while(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_dowhile(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static inline void compile_skip(cout_t*, struct bytechunk*);
-static inline void compile_stop(cout_t*, struct bytechunk*);
-static void compile_unary(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_binary(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_logical_binary(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_val(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_func_call(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_assign(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_var_ref(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_func_def(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_return(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_list(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_subscript(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_map(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
-static void compile_memacc(cout_t*, ast_t*, hashmap_t*, struct bytechunk*);
+static void compile_node(cout_t*, ast_t*, struct scope*); /* compile any node into bytecode */
+static void compile_if(cout_t*, ast_t*, struct scope*);
+static void compile_while(cout_t*, ast_t*, struct scope*);
+static void compile_dowhile(cout_t*, ast_t*, struct scope*);
+static inline void compile_skip(cout_t*, struct scope*);
+static inline void compile_stop(cout_t*, struct scope*);
+static void compile_unary(cout_t*, ast_t*, struct scope*);
+static void compile_binary(cout_t*, ast_t*, struct scope*);
+static void compile_logical_binary(cout_t*, ast_t*, struct scope*);
+static void compile_val(cout_t*, ast_t*, struct scope*);
+static void compile_func_call(cout_t*, ast_t*, struct scope*);
+static void compile_assign(cout_t*, ast_t*, struct scope*);
+static void compile_var_ref(cout_t*, ast_t*, struct scope*);
+static void compile_func_def(cout_t*, ast_t*, struct scope*);
+static void compile_return(cout_t*, ast_t*, struct scope*);
+static void compile_list(cout_t*, ast_t*, struct scope*);
+static void compile_subscript(cout_t*, ast_t*, struct scope*);
+static void compile_map(cout_t*, ast_t*, struct scope*);
+static void compile_memacc(cout_t*, ast_t*, struct scope*);
 
 #endif /* SEAL_COMPILER_H */
